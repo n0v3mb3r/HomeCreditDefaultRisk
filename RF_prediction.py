a89337dd9d6770dbb6b2419ea970fb854a6bc4f8
Wayne <sansugui@gmail.com>
2018-06-03 15:58:32 -0700

    Added RF prediction code

--- RF_prediction.py
+++ RF_prediction.py
@@ -8,29 +8,28 @@ data.import_train_data()
 data.application_train = ida.data_imputer().fit_transform(data.application_train)
 data.application_train = pd.get_dummies(data.application_train)
 
-# create features and target variable
-features = data.application_train.columns[1:(len(data.application_train.columns) - 1)]
-target_variable = data.application_train.columns[len(data.application_train.columns) - 1]
-
-train = pd.get_dummies(data.application_train[features])
-labels = pd.get_dummies(data.application_train[target_variable])
-
-print('Training Random Forest Classifier')
-rfc = RandomForestClassifier(n_estimators = 500)
-rfc.fit(train, labels)
-
-print('Generation Predictions')
-
 # Import Test Dataset
 data.import_test_data()
 data.application_test = ida.data_imputer().fit_transform(data.application_test)
 data.application_test = pd.get_dummies(data.application_test)
 
+# create features and target variable
+features = data.application_test.columns[1:len(data.application_test.columns)]
+target_variable = 'TARGET'
+
+train = pd.get_dummies(data.application_train[features])
+labels = pd.get_dummies(data.application_train[target_variable])
+
+print('Training Random Forest Classifier')
+rfc = RandomForestClassifier(n_estimators = 500, verbose = True)
+rfc.fit(train, labels)
+
+print('Generation Predictions')
 test = pd.get_dummies(data.application_test[features])
 
 predictions = pd.DataFrame(rfc.predict(test))
 
-export = pd.concat(test["SK_ID_CURR"], predictions, axis = 1)
+export = pd.concat(data.application_test["SK_ID_CURR"], predictions, axis = 1)
 export_headers = ["SK_ID_CURR","TARGET"]
 export.columns = export_headers
 
